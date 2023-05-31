0e2a235d95434d9f122885840ec5f9d4efaa8202
Gabriel Gatzsche <gabriel.gatzsche@inlavigo.com>
2023-05-31 13:53:24 +0200

    Fix: Xml documents with multiple score parts were not read anymore

--- test/score_parts_test.dart
+++ test/score_parts_test.dart
@@ -9,11 +9,7 @@ final emptyScoreParts = File('test/assets/emptyScoreParts.xml');
 void main() {
   test('MultipleScoreParts', () {
     expect(() => MusicXmlDocument.parse(multipleScoreParts.readAsStringSync()),
-        throwsA(isA<StateError>()));
-    expect(
-        () => MusicXmlDocument.parse(multipleScoreParts.readAsStringSync()),
-        throwsA(predicate(
-            (e) => e.toString().contains('Bad state: Too many elements'))));
+        returnsNormally);
   });
   test('EmptyScoreParts', () {
     final document = MusicXmlDocument.parse(emptyScoreParts.readAsStringSync());
