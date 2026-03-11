cf6289abb3cbe0660507a410b80a0768fe69a500
Thai <thaihuynhxyz@gmail.com>
2026-03-11 13:49:39 +0700

    fix: remove duplicate makeTime parse in Grace.parse (#24)
    The makeTime attribute was parsed twice - once by a leftover if
    statement and again by the switch case. Added test for make-time.

--- test/grace_test.dart
+++ test/grace_test.dart
@@ -4,15 +4,28 @@ import 'package:music_xml/music_xml.dart';
 import 'package:test/test.dart';
 
 // https://www.w3.org/2021/06/musicxml40/musicxml-reference/examples/grace-element-appoggiatura/
-final asset = File('test/assets/grace-element-appoggiatura.xml');
+final appoggiatura = File('test/assets/grace-element-appoggiatura.xml');
+final makeTimeAsset = File('test/assets/grace-element-make-time.xml');
 
 void main() {
   test('<grace> (Appoggiatura)', () {
-    final document = MusicXmlDocument.parse(asset.readAsStringSync());
+    final document = MusicXmlDocument.parse(appoggiatura.readAsStringSync());
 
     final grace =
         document.score.parts.single.measures.single.notes.single.grace;
     expect(grace!.slash!.yesNo, isFalse);
     expect(grace.stealTimeFollowing!.percent, 33);
+    expect(grace.makeTime, isNull);
+  });
+
+  test('<grace> with make-time', () {
+    final document = MusicXmlDocument.parse(makeTimeAsset.readAsStringSync());
+
+    final grace =
+        document.score.parts.single.measures.single.notes.single.grace;
+    expect(grace!.makeTime!.divisions, 100);
+    expect(grace.slash!.yesNo, isTrue);
+    expect(grace.stealTimeFollowing, isNull);
+    expect(grace.stealTimePrevious, isNull);
   });
 }
